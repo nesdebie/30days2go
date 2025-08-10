c185d62ff19d5f827b51cabe92a0e0d665cd8580
nesdebie <ndebiesm@gmail.com>
2025-08-10 20:57:25 +0200

    multi-ping

---
+++ 23.Concurrencial-Multi-Ping/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "sync"
+    "time"
+	"os"
+)
+
+type PingResult struct {
+    URL      string
+    Duration time.Duration
+    Error    error
+}
+
+
+func ping(url string, waitGroup *sync.WaitGroup, results chan<- PingResult) {
+    defer waitGroup.Done()
+    start := time.Now()
+    resp, err := http.Get(url)
+    duration := time.Since(start)
+    if resp != nil {
+        resp.Body.Close()
+    }
+    results <- PingResult{URL: url, Duration: duration, Error: err}
+}
+
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: " + os.Args[0] + "<url1> <url2> ... <url_N>")
+		fmt.Println("Example: " + os.Args[0] + " https://google.com https://github.com/nesdebie" )
+		os.Exit(1)
+	}
+
+    urls := os.Args[1:]
+
+    var waitGroup sync.WaitGroup
+    results := make(chan PingResult, len(urls))
+
+    for _, url := range urls {
+        waitGroup.Add(1)
+        go ping(url, &waitGroup, results)
+    }
+
+    go func() {
+        waitGroup.Wait()
+        close(results)
+    }()
+
+    for result := range results {
+        if result.Error != nil {
+            fmt.Printf("Erreur pour %s: %v\n", result.URL, result.Error)
+        } else {
+            fmt.Printf("Ping vers %s : %v\n", result.URL, result.Duration)
+        }
+    }
+}
